d2386bcc020845a908029d8b126c4c2e5990971d
chinmaysubandh <91725938+chinmaysubandh@users.noreply.github.com>
2025-07-02 03:35:48 +0530

    Home page stuff

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className='px-4 md:px-8 lg:px-16 lx:px-32 2xl:px-64'>
       {/* Navbar */}
-      <Navbar />
+      {/* <Navbar /> */}
       {/* Breadcrums */}
       {/* introduction */}
       {/* Post list */}

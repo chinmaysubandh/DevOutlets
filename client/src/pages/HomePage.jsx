b3e3355c197b6bf4bf84a25cb3d1257c5e13dfe1
chinmaysubandh <91725938+chinmaysubandh@users.noreply.github.com>
2025-07-02 04:13:15 +0530

    animated button position corrected

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -21,12 +21,12 @@ const HomePage = () => {
 
         </div>
         {/* animated button */}
-        <Link to='/write'>
+        <Link to='/write' className=' relative'>
           <svg
             viewBox="0 0 200 200"
             width="200"
             height="200"
-            className='text-lg tracking-widest relative'
+            className='  text-lg tracking-widest'
             
           >
             <path id="circlePath"
@@ -38,24 +38,24 @@ const HomePage = () => {
               <textPath href="#circlePath" startOffset="0%">Write your Story..</textPath>
               <textPath href="#circlePath" startOffset="50%">Share your Idea..</textPath>
             </text>
-            <button className='rounded-full absolute top-0 left-0 right-0 bottom-0 h-20 w-20 m-auto flex items-center justify-center bg-blue-800'>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
+                       
+          </svg>
+          <button className='rounded-full absolute top-0 left-0 right-0 bottom-0 h-20 w-20 m-auto flex items-center justify-center bg-blue-800'>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
 
-                viewBox="0 0 24 24"
-                width="50"
-                height="50"
-                fill="none"
-                stroke="white"
-                strokeWidth="2"
-              >
-                <line x1="6" y1="18" x2="18" y2="6" />
-                <polyline points="9 6 18 6 18 15" />
-              </svg>
-              
-            </button>
-            
-        </svg>
+              viewBox="0 0 24 24"
+              width="50"
+              height="50"
+              fill="none"
+              stroke="white"
+              strokeWidth="2"
+            >
+              <line x1="6" y1="18" x2="18" y2="6" />
+              <polyline points="9 6 18 6 18 15" />
+            </svg>
+
+          </button>
         </Link>
 
       </div>
